ec8b2f2cb7ff3bfaf2c3029a3a7efbdbfaa6db8f
Valodzina <ulyanavalodzina@gmail.com>
2021-12-20 11:07:25 +0300

    start rewrite project
    add jquery, slick slider. start using scss

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
+$(function() { 
+    $('.slick__slider').slick({
+        arrows:true,
+        dots: false,
+        prevArrow: "<img src='images/left.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='images/right.svg' class='next' alt='2'>",
+        infinite: true,
+		autoplay: true,
+        autoplaySpeed:3000,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                   // infinite: false,
+                   // arrows: true,
+                   // dots: false
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            } ]
+        
+    }) 
+});
+
+ $(function() { 
+    $(".btn").click(function () {
+		$(".hide__part").slideToggle("slow");
+	
+		if (this.innerHTML == "Показать меньше") {
+			this.innerHTML = "Показать больше";
+			return;
+		}
+		else this.innerHTML = "Показать меньше";
+	
+	
+	})
+ });
+
+
 
 
 
